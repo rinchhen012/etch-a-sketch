3cef3fd700393243305537c1b326a77e75d4cd2a
rinchhen012 <rinchhen.meh@gmail.com>
2020-02-22 17:05:49 +0900

    Final commit v1.0

--- index.js
+++ index.js
@@ -8,33 +8,37 @@ custom();
 const gridSwitch = document.querySelector('.remove-grid');
 gridSwitch.addEventListener('click', gridSwitcher);
 
-// reset entire container
+// resets entire container => bgColor / opacity
 const reset = document.querySelector('.reset');
 reset.addEventListener('click', () => {
     const boxes = Array.from(document.querySelectorAll('.box'));
     boxes.forEach(box => {
         box.style.backgroundColor = defaultColor;
+        box.style.opacity = 1;
     })
 });
 
 listeners();
+
 // mousedown event listener to call move() & eraser  
 function listeners() {
     const boxes = Array.from(document.querySelectorAll('.box'));
     boxes.forEach(box => {
         box.addEventListener('mousedown', event => {
             if (event.button === 0) {
-                box.style.backgroundColor = '#FFD662';
                 event.preventDefault();
                 container.addEventListener('mousemove', move);
                 container.addEventListener('mousemove', event => {
-                    if (event.button === 0 && event.ctrlKey)
+                    if (event.button === 0 && event.ctrlKey) {
                         event.target.style.backgroundColor = defaultColor;
+                        event.target.style.opacity = 1;
+                    }
                 })
             }
         })
     })
 }
+
 // changes to custom grid layout using user input
 function custom() {
     const custom = document.querySelector('.custom');
@@ -54,50 +58,46 @@ function custom() {
         }
     });
 }
+
 // get random rgb values
 function randomRgb() {
     return Math.floor(Math.random() * 256);
 }
-// get black values
-function addBlack(rgb) {
-    let r = Math.round(rgb[0] * 0.7);
-    let g = Math.round(rgb[1] * 0.7);
-    let b = Math.round(rgb[2] * 0.7);
-    return [r, g, b];
-}
+
 // switches gridline 1/0
 function gridSwitcher() {
     const boxes = Array.from(document.querySelectorAll('.box'));
-    if (event.target.textContent == 'Remove Gridlines') {
-        event.target.textContent = 'Show Gridlines';
-        for (box of boxes)
-            box.style.border = 'none';
-    }
-    else {
+    if (event.target.textContent == 'Show Gridlines') {
         event.target.textContent = 'Remove Gridlines';
-        for (let box of boxes)
+        for (box of boxes)
             box.style.border = '1px solid rgb(71, 71, 71)';
     }
+    else {
+        event.target.textContent = 'Show Gridlines';
+        for (let box of boxes)
+            box.style.border = 'none';
+    }
 }
+
 // creates divs
 function gridCreate(x) {
     for (let i = 0; i < x; i++) {
         let div = document.createElement('div');
         container.appendChild(div);
         div.className = 'box';
+        div.style.opacity = "1";
     }
 }
+
 // colors the box when mouse is moved
 function move() {
     // Removes colorize on hover effect.
     if (event.buttons === 0)
         container.removeEventListener('mousemove', move);
 
-    let boxColor = event.target.style.backgroundColor;
-    let boxOpacity = event.target.style.opacity;
+    event.target.style.backgroundColor = `rgb(${randomRgb()},${randomRgb()},${randomRgb()})`;
 
-    if (boxColor == "")
-        event.target.style.backgroundColor = `rgb(${randomRgb()},${randomRgb()},${randomRgb()})`;
+    let boxOpacity = event.target.style.opacity;
 
     if (boxOpacity > 0)
         event.target.style.opacity = boxOpacity - 0.1;
